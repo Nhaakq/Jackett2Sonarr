8248c1a2b988c7d56155422b397a11dcf444b863
Nhaakq <Contact@nhaak.com>
2022-03-26 09:58:49 +0100

    Working Script

--- Jackett2Sonarr.py
+++ Jackett2Sonarr.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, json
 import os.path
 import sys
 from os import listdir
@@ -6,39 +6,57 @@ from os.path import isfile, join
 from sqlite3 import Error
 
 def insert_datas(database, task):
-    """
-    Create a new task
-    :param conn:
-    :param task:
-    :return:
-    """
-    #database = r"C:/Users/Nathan/Documents/Scripts/Python/SUTOM/dictionnaire.db"
     try:
-        conn = sqlite3.connect(database)
+        conn = sqlite3.connect(database,timeout=10)
     except Error as e:
         print(e)
 
     sql = ''' INSERT OR IGNORE INTO Indexers (Name,"Implementation",Settings,ConfigContract,EnableRss,EnableAutomaticSearch,EnableInteractiveSearch,Priority,Tags,DownloadClientId)
-             VALUES (?,?,?,?,?,?)'''
+             VALUES (?,?,?,?,?,?,?,?,?,?)'''
     cur = conn.cursor()
     cur.execute(sql, task)
     conn.commit()
 
+def update_datas(database, sql_update):
+    try:
+        conn = sqlite3.connect(database,timeout=10)
+    except Error as e:
+        print(e)
+
+    cur = conn.cursor()
+    cur.execute(sql_update)
+    conn.commit()
+
+def check_names(database, sql_update):
+    try:
+        conn = sqlite3.connect(database,timeout=10)
+    except Error as e:
+        print(e)
+
+    cur = conn.cursor()
+    cur.execute(sql_update)
+    export_line = cur.fetchall()
+    conn.commit()
+    return export_line
 
 def main():
     av_files = []
+    user_indexers = []  
     path_sonarr = input('Sonnar.db path ? :') or 'scripts/Jackett2Sonarr/sonarr.db'
     path_jackett = input('Jackett configuration path ? :') or 'scripts/Jackett2Sonarr/Jackett/'
     path_indexers = join(path_jackett,'Indexers')
     choice_all = input('Do you want to import all Indexers ? (y/n)') or 'n'
 
+    f = open ((path_jackett+'/ServerConfig.json'), "r")
+    data = json.loads(f.read())
+    url_jackett = data['BaseUrlOverride']
+    api_key = data['APIKey']
+    f.close()
+
     try:
-        conn = sqlite3.connect(path_sonarr)
+        conn = sqlite3.connect(path_sonarr,timeout=10)
     except Error as e:
         print(e)
-      
-    #print(path_sonarr)
-    #print(path_jackett)
 
     onlyfiles = [f for f in listdir(path_indexers) if (isfile(join(path_indexers, f)) and (f.endswith('.json')))]
 
@@ -50,38 +68,30 @@ def main():
         compteur += 1
     
     if choice_all in 'yY':
-        print('IMPORT ALL')
+        for index_name in av_files:
+            sql2 = "SELECT * FROM Indexers WHERE Name='"+index_name+"';"
+            all_line = check_names(path_sonarr,sql2)
+            if not all_line :
+                sql_insert = (index_name,'Newznab','''{"baseUrl": "'''+url_jackett+'''/api/v2.0/indexers/'''+index_name+'''/results/torznab/","apiPath": "/api","apiKey": "'''+api_key+'''","categories": [],"animeCategories": [],"animeStandardFormatSearch": true}''','NewznabSettings',1,1,1,25,'[]',0)            
+                insert_datas(path_sonarr,sql_insert)
+            else:
+                sql_update=('''Update Indexers set Name = "'''+index_name+'''", Settings = '{"baseUrl": "'''+url_jackett+'''/api/v2.0/indexers/'''+index_name+'''/results/torznab/","apiPath": "/api","apiKey": "'''+api_key+'''","categories": [],"animeCategories": [],"animeStandardFormatSearch": true}'  where Name = "'''+index_name+'''";''')
+                update_datas(path_sonarr,sql_update)
         exit()
-    #print(onlyfiles[2])
-    lst1 = []    
-    lst1 = [int(item) for item in input("Enter the Indexers number separate with spaces : ").split()]
-
-    for select in lst1:
+    
+    user_indexers = [int(item) for item in input("Enter the Indexers number separate with spaces : ").split()]
+    for select in user_indexers:
         index_name = av_files[select]
         sql2 = "SELECT * FROM Indexers WHERE Name='"+index_name+"';"
-        cur = conn.cursor()
-        cur.execute(sql2)
-        #conn.commit()
-        all_line=cur.fetchall()
+        all_line = check_names(path_sonarr,sql2)
         if not all_line :
-            print('c vide')
-            sql_insert = (index_name,'Newznab','{
-            "baseUrl": "https://jTESTTESTTEST/",
-            "apiPath": "/api",
-            "apiKey": "16qks88lejde3b1z2oz74zcxtdxptqud",
-            "categories": [
-                5000,
-                5040,
-                5050,
-                100208
-            ],
-            "animeCategories": [],
-            "animeStandardFormatSearch": true
-            }','NewznabSettings',1,1,1,25,'[]',0))
-            print(sql_insert)
-            #insert_datas(path_sonarr,)
-
-
+            sql_insert = (index_name,'Newznab','''{"baseUrl": "'''+url_jackett+'''/api/v2.0/indexers/'''+index_name+'''/results/torznab/","apiPath": "/api","apiKey": "'''+api_key+'''","categories": [],"animeCategories": [],"animeStandardFormatSearch": true}''','NewznabSettings',1,1,1,25,'[]',0)            
+            #print(sql_insert)
+            insert_datas(path_sonarr,sql_insert)
+        else:
+            sql_update=('''Update Indexers set Name = "'''+index_name+'''", Settings = '{"baseUrl": "'''+url_jackett+'''/api/v2.0/indexers/'''+index_name+'''/results/torznab/","apiPath": "/api","apiKey": "'''+api_key+'''","categories": [],"animeCategories": [],"animeStandardFormatSearch": true}'  where Name = "'''+index_name+'''";''')
+            update_datas(path_sonarr,sql_update)
+            #print(sql_update)
 
 if __name__ == '__main__':
     main()
